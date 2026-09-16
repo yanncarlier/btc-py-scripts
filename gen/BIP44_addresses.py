0d23dd97fbc7eda2c020e59d8beb6c29524cecda
Yann <227698+yanncarlier@users.noreply.github.com>
2026-09-16 16:35:30 +0700

    json output for gen/BIP44_addresses.py

--- gen/BIP44_addresses.py
+++ gen/BIP44_addresses.py
@@ -2,6 +2,8 @@
 Generates Native SegWit (P2WPKH) Addresses.
 BIP44 (Legacy P2PKH, among others) uses 44'.
 '''
+import json
+
 from bip_utils import Bip39SeedGenerator, Bip44, Bip44Coins, Bip44Changes, Bip39MnemonicValidator
 from bip_utils.utils.mnemonic import MnemonicChecksumError
 from derivation_cli import parse_derivation_arguments
@@ -18,25 +20,16 @@ try:
     if not Bip39MnemonicValidator().IsValid(mnemonic):
         raise ValueError("Invalid mnemonic phrase provided. Please check the words and try again.")
     
-    print("Mnemonic Phrase:", mnemonic)
-    print("Passphrase:", passphrase if passphrase else "<empty>")
-
     # Generate seed from mnemonic with passphrase
     seed_bytes = Bip39SeedGenerator(mnemonic).Generate(passphrase=passphrase)
 
-    # Display the generated seed (in hex)
-    print("Seed (hex):", seed_bytes.hex())
-
     # Initialize BIP44 for Bitcoin mainnet and derive the default account (m/44'/0'/0')
-    print("Generating Native SegWit (P2WPKH) Addresses:")
-
     bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.BITCOIN)
 
     bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0)
 
-    # Print the Account Extended Public Key (xpub)
     account_xpub = bip44_acc_ctx.PublicKey().ToExtended()
-    print("Account Extended Public Key (xpub):", account_xpub)
+    addresses = []
 
     # Generate a set number of addresses
     for i in range(num_addresses):
@@ -58,14 +51,29 @@ try:
         private_key = bip44_addr_ctx.PrivateKey().Raw().ToHex()
         wif = bip44_addr_ctx.PrivateKey().ToWif()  # Private key in WIF format
 
-        # Print the output in the specified order
-        print("{")
-        print(f"derivation_path: {derivation_path}")
-        print(f"address: {address}")
-        print(f"public_key: {public_key}")
-        print(f"private_key: {private_key}")
-        print(f"wif: {wif}")
-        print("},")
+        addresses.append(
+            {
+                "derivation_path": derivation_path,
+                "address": address,
+                "public_key": public_key,
+                "private_key": private_key,
+                "wif": wif,
+            }
+        )
+
+    print(
+        json.dumps(
+            {
+                "mnemonic_phrase": mnemonic,
+                "passphrase": passphrase,
+                "seed_hex": seed_bytes.hex(),
+                "address_type": "BIP44 P2PKH",
+                "account_extended_public_key": account_xpub,
+                "addresses": addresses,
+            },
+            indent=2,
+        )
+    )
 
 except MnemonicChecksumError as e:
     print(f"Error: Invalid mnemonic checksum. Details: {e}")
