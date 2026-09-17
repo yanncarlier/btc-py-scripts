dc993db46c6d5aad663576f290d21ad62f99ad02
Yann <227698+yanncarlier@users.noreply.github.com>
2026-09-17 08:45:16 +0700

    Update output to JSON format in generate_mnemonic.py

--- gen/generate_mnemonic.py
+++ gen/generate_mnemonic.py
@@ -1,4 +1,5 @@
 # https://github.com/trezor/python-mnemonic
+import json
 import argparse
 from mnemonic import Mnemonic
 import bip32utils  # Ensure you have this library installed
@@ -15,19 +16,23 @@ def main():
     args = parser.parse_args()
 
     mnemonic = Mnemonic("english")
-    print('++++++++++++++++++++++++++++++++++++++++++++++')
+    
     # Generate word list given the strength (128 - 256):
     words = mnemonic.generate(strength=args.strength)
-    print('BIP39 Mnemonic: %s' % words)
-    print('++++++++++++++++++++++++++++++++++++++++++++++')
+    
     # Given the word list generate seed:
     seed = mnemonic.to_seed(words)
-    print('BIP39 Seed: %s' % seed.hex())
-    print('++++++++++++++++++++++++++++++++++++++++++++++')
+    
     # Generate BIP32 Root Key:
     bip32_root_key = bip32utils.BIP32Key.fromEntropy(seed)
-    print('BIP32 Root Key: %s' % bip32_root_key.ExtendedKey())
-    print('++++++++++++++++++++++++++++++++++++++++++++++')
+    
+    output = {
+        "bip39_mnemonic": words,
+        "bip39_seed": seed.hex(),
+        "bip32_root_key": bip32_root_key.ExtendedKey()
+    }
+    
+    print(json.dumps(output, indent=4))
 
 if __name__ == "__main__":
     main()
